e97dd51f62acdebdde338d73d1c34567ca2132d4
gannat-123 <gannata79@gmail.com>
2025-07-02 10:21:31 +0300

    add google site verification in metadata

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://your-domain.com", // ← غيري ده لرابط موقعك الفعلي
+  generateRobotsTxt: true, // ← هينشئ ملف robots.txt كمان تلقائيًا
+};
